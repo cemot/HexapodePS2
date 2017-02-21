29831179ccf9285599b9d3e4adc93a415ea5edf4
Gregoire JOUAN <gregoire.jouan@gmail.com>
2017-02-21 21:46:19 +0100

    first version
    Coming from https://github.com/feltrine/Hexapode and https://github.com/gregmuch/PS2_controller

---
+++ hexaWiFi.cpp
@@ -0,0 +1,50 @@
+#include "hexaWiFi.h"
+
+ESP8266WebServer server(80);
+
+void initWiFi()
+{
+  WiFi.softAP( _SSID_, _PASSWORD_ );
+  server.on( "/", handleRoot );
+  server.on( "/up", handleForward );
+  server.on( "/down", handleBackward );
+  server.on( "/left", handleLeft );
+  server.on( "/right", handleRight );
+  server.begin();
+}
+
+void waitInstr()
+{
+  server.esp8266 handleClient();
+}
+
+void handleRoot()
+{
+  hexaHoming();
+  server.send(200, "text/html", "<h1>&emsp;&emsp;&emsp;&emsp;<a href=\"/up\">up</a><br/><a href=\"/left\">left</a>&emsp;<a href=\"/\">home</a>&emsp;<a href=\"/right\">right</a><br/>&emsp;&emsp;&emsp;<a href=\"/down\">down</a><br/></h1>");
+}
+
+void handleForward()
+{
+  hexaMove( 1, 0, 1, 0 );
+  server.send(200, "text/html", "<h1>&emsp;&emsp;&emsp;&emsp;<a href=\"/up\">up</a><br/><a href=\"/left\">left</a>&emsp;<a href=\"/\">home</a>&emsp;<a href=\"/right\">right</a><br/>&emsp;&emsp;&emsp;<a href=\"/down\">down</a><br/></h1>");
+}
+
+void handleBackward()
+{
+  hexaMove( -1, 0, 1, 0 );
+  server.send(200, "text/html", "<h1>&emsp;&emsp;&emsp;&emsp;<a href=\"/up\">up</a><br/><a href=\"/left\">left</a>&emsp;<a href=\"/\">home</a>&emsp;<a href=\"/right\">right</a><br/>&emsp;&emsp;&emsp;<a href=\"/down\">down</a><br/></h1>");
+}
+
+void handleLeft()
+{
+  hexaRotate( 1, 1, 0 );
+  server.send(200, "text/html", "<h1>&emsp;&emsp;&emsp;&emsp;<a href=\"/up\">up</a><br/><a href=\"/left\">left</a>&emsp;<a href=\"/\">home</a>&emsp;<a href=\"/right\">right</a><br/>&emsp;&emsp;&emsp;<a href=\"/down\">down</a><br/></h1>");
+}
+
+void handleRight()
+{
+  hexaRotate( -1, 1, 0 );
+  server.send(200, "text/html", "<h1>&emsp;&emsp;&emsp;&emsp;<a href=\"/up\">up</a><br/><a href=\"/left\">left</a>&emsp;<a href=\"/\">home</a>&emsp;<a href=\"/right\">right</a><br/>&emsp;&emsp;&emsp;<a href=\"/down\">down</a><br/></h1>");
+}
+
